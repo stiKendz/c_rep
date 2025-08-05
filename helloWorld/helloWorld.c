bdae9f0ba5d743e20a405b4913f6635dd80008b6
secondHomePC <toguedriver@gmail.com>
2025-08-05 14:45:17 +0300

    Загрузка первых проектов написанных на C

---
+++ helloWorld/helloWorld.c
@@ -0,0 +1,46 @@
+﻿#include <stdio.h>
+
+/*int main() {
+		printf("Hello World\n");
+		return 0;
+	}
+
+int main() {
+		int number = 10;
+		printf("The number is %d\n", number);
+		return 0;	
+	}
+
+
+int main() {
+		int number;
+		int numberTwo = 12;
+		printf("Enter a number: ");
+		scanf("%d", &number);
+		printf("You entered: %d and number two (not entered) %d\n", number, numberTwo);
+		return 0;
+	}
+
+
+int main() {
+		int number, numberTwo;
+		printf("Print first number: ");
+		scanf("%d", &number);
+		printf("Print second number: ");
+		scanf("%d", &numberTwo);
+		int sum = number + numberTwo;
+		printf("Summa vvedennix chisel ravna = %d\n. Pobeda! \n", sum);
+	}
+*/
+
+int main() {
+		int number;
+		printf("Print number: ");
+		scanf("%d", &number);
+		if ( number % 2 == 0) {
+			printf("The number even.\n");
+		} else {
+			printf("The number id odd.\n");
+		}
+		return 0;
+	}
---
+++ simpleCalculator/calculator.c
@@ -0,0 +1,40 @@
+﻿#define _CRT_SECURE_NOWARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+	double r1=0, r2=0, r3=0;
+	char op;
+
+	printf("Enter first number \n");
+	scanf(" %lf", &r1);
+
+	printf("Enter operator \n");
+	scanf(" %c", &op);
+	printf("Enter second number\n");
+	scanf(" %lf", &r2);
+
+	switch(op) {
+		case '+': r3 = r1 + r2; break;
+		case '-': r3 = r1 - r2; break;
+		case '*': r3 = r1 * r2; break;
+		case '/': 
+			if ( fabs(r2) < 1.0E-10 ) {
+				printf("Divide by zero!\n");
+				scanf(" %c", &op);
+				return 1;
+			}
+			r3 = r1 / r2;
+			break;
+		default:
+			printf("Error!\n");
+			scanf(" %c", &op);
+			return 2;
+	}
+	printf("%f %c %f = %f\n", r1, op, r2, r3);
+	scanf(" %c", &op);
+	return 0;
+}
