4a084b671aa2c1d94aa29625eb1c21f304b583c2
= <=>
2025-08-31 18:09:23 +0300

    Переворошил проект-тренировку, прописав в главном файле типы указателей.

--- training/training.c
+++ training/training.c
@@ -1,249 +1,63 @@
 ﻿#include <stdio.h>
+#include <stdlib.h>
 #include <stddef.h>
 
-/*int voidSign()
+// NULL pointer
+/* НУЛЕВЫЕ указатели обычно используются для обозначения отсутствия какого-либо адреса. 
+Это позволяет нам проверить, указывает ли указатель на какую-либо допустимую ячейку памяти, проверив, равно ли оно NULL.*/
+int nullPtr()
 {
-	int a = 123;
-	int *ip = &a;
-
-	void *vp;
-	vp=ip;
-
-	printf("Value: %d\n", *((int *)vp));
+	int *ptr = NULL;
 
 	return 0;
 }
 
-int voidSignTwo()
+// Void pointer
+/* Указатели void в C являются указателями типа void. 
+Это означает, что они не имеют никакого связанного типа данных. 
+Их также называют универсальными указателями, поскольку они могут указывать на любой тип и могут быть преобразованы в любой тип.*/
+int voidPtr()
 {
-	int a = 123;
-	int *ip = &a;
-
-	void *vp = ip;
-
-	printf("vp: %p\n", vp);
-	printf("ip: %p\n", (void*)ip);
+	void *ptr;
 
 	return 0;
 }
 
-int signAdress()
-{
-	int a = 12;
-	int *pa = &a;
-	printf("adress of pointer = %p\n", (void*)&pa); //адрес указателя 
-	printf("adress in pointer = %p\n", (void*)pa); //адрес который хранится в указателе
-	printf("value on pointer = %d\n", *pa); //значение по адресу в указателе
-
-	return 0;
-}
-
-int signComparison()
-{
-	int a = 10;
-	int b = 20;
-
-	int *pa = &a;
-	int *pb = &b;
-
-	if(pa>pb)
-		printf("pa (%p) bolshe chem pb (%p) \n", (void*)pa, (void*)pb);
-	else
-		printf("pb (%p) bolshe ili ravno pa (%p) \n", (void*)pb, (void*)pa);
-
-
-	return 0;
-}
-
-int types()
-{
-	char c = 'N';
-	char *pc = &c;
-	int *pd = (int*)pc;
-
-	printf("pc=%d\n", (void*)pc);
-	printf("pd=%d\n", (void*)pd);
-
-	return 0;
-}
-
-int checkWork()
-{
-	int a = 12;
-	int *pa = &a;
-	printf("Exercise 1.\n Adress of a = %p\t Value of a %d\n", (void*)pa, *pa);
-
-	int b = 21;
-	int *ba = &b;
-	printf("b before transformation %d\n", *ba);
-
-	*ba = 25;
-	printf("b after transformation %d\n", *ba);
-
-	return 0;
-}
+// Wild pointers
+/* Подстановочные указатели - это указатели, которые еще не были инициализированы каким-либо образом. 
+Эти типы C-указателей могут вызывать проблемы в наших программах и в конечном итоге приводить к их аварийному завершению. 
+Если значения обновляются с использованием подстановочных указателей, это может привести к прерыванию работы или повреждению данных.
 */
-
-
-/*В случае с указателем увеличение на единицу будет означать увеличение адреса, 
-который хранится в указателе, на размер типа указателя.
-Фактически увеличение на единицу означает, что мы хотим перейти к следующему объекту в памяти, 
-который находится за текущим и на который указывает указатель. 
-А уменьшение на единицу означает переход назад к предыдущему объекту в памяти.
-*/
-int incrementDecrement()
+int wildPtr()
 {
-	int a = 12;
-
-	int *ptr = &a;
-	printf("adress = %p\t value=%d\n", (void*)ptr, *ptr);
-	ptr++;
-	printf("adress = %p\t value=%d\n", (void*)ptr, *ptr);
-	ptr--;
-	printf("adress = %p\t value=%d\n", (void*)ptr, *ptr);
-
-
-	printf("double and char\n");
-	double b = 10.6;
-	double *pb = &b;
-
-	printf("Pointer pd: adress=%p\n", (void*)pb);
-	pb++;
-	printf("Pointer pd: adress=%p\n", (void*)pb);
-
-
-	char c = 'N';
-	char *pc = &c;
-	printf("Pointer pc: adress=%p\n", (void*)pc);
-	pc++;
-	printf("Pointer pc: adress=%p\n", (void*)pc);
-
-
-
-	printf("Adresses\n");
-	double d = 10.6;
-	double *pd = &d;
-	printf("Pointer pd: adress=%p\n", (void*)pd);
-	pd = pd + 2;
-	printf("Pointer pd: adress=%p\n", (void*)pd);
-
-
-
-	char cc = 'N';
-    char *pcc = &cc;
-    printf("Pointer pc: address=%p \n", (void*)pc);
-    pcc = pc - 3;
-    printf("Pointer pc: address=%p \n", (void*)pc);
-
-
-
-	printf("4 bytes difference\n");
-	int e = 10;
-	int f = 22;
-	int *pe = &e;
-	int *pf = &f;
-	ptrdiff_t g = pe - pf;
-
-	printf("pe=%p\n",(void*)pe);
-	printf("pf=%p\n",(void*)pf);
-	printf("g=%lld\n",g);
+	int *ptr;
 
 	return 0;
 }
 
-int arrayDiff()
+// Dangling pointers
+/*Указатель, указывающий на область памяти, которая была удалена (или освобождена), называется висячим указателем. 
+Такая ситуация может привести к неожиданному поведению программы, а также послужить источником ошибок в программах на языке Си.*/
+int danglingPtr()
 {
-	/* Расстояние между указателями представляет тип ptrdif_t - на 64-разрядной архитектуре 
-	этот тип является псевдонимом для базового типа long long и занимает 8 байт. 
-	*/
-	int arr[64];
-	int* ptr1 = &arr[10];
-	int* ptr2 = &arr[40];
-	ptrdiff_t dist = ptr2 - ptr1;
+	int* ptr = (int*)malloc(sizeof(int));
+	
+	free(ptr);
+	printf("Memory freed \n");
 
-	printf("dist=%lld\n", dist);
-
-	return 0;
-
-}
-
-int signSpecific()
-{
-	int a = 10;
-	int *pa = &a;
-	int b = *pa + 20; // операция со значением, на который указывает указатель
-	pa++; // операция с самим указателем (прейти к другому уровню в памяти?)
-
-	printf("b = %d\n", b); //30
-
-
-	int x = 10;
-	int *xp = &x;
-	printf("xp: address=%p \t value=%d \n", (void*)xp, *xp);
-	int y = *xp++;     // инкремент адреса указателя
-	int yt =(*xp)++;
-
-	printf("y: value=%d \n", y);
-	printf("yt: value=%d \n", yt);
-
-	printf("xp: address=%p \t value=%d \n", (void*)xp, *xp);
+	ptr = NULL; //Указатель ptr явно устанавливается в NULL, чтобы избежать неопределенного поведения.
 
 	return 0;
 }
 
-int forCheck()
-{
-	int a = 10;
-	int *pa = &a;
-	printf("pa: address=%p \t value=%d \n", (void*)pa, *pa);
-	int b = *pa++;      // инкремент адреса указателя
-	     
-	printf("b: value=%d \n", b);
-	printf("pa: address=%p \t value=%d \n", (void*)pa, *pa);
-
-	return 0;
-}
-
-
+// Константа и указатель
 int main(void)
 {
-    /*int a = 12;
-    int b = 6;
-     
-    int *pa = &a;
-    int *pb = &b;
-     
-    printf("Variable a: address=%p \t value=%d \n", (void*)pa, *pa);
-    printf("Variable b: address=%p \t value=%d \n", (void*)pb, *pb);
-     
-    pa = pb;    // теперь указатель pa хранит адрес переменной b
-    printf("Variable b: address=%p \t value=%d \n", (void*)pa, *pa);
+	const int cx = 10;
+	*(int*) &cx = 20;	
+	printf("cx: %d\n", cx);
 
-	voidSign();
+	danglingPtr();
 
-	voidSignTwo();
-
-	signAdress();     
-
-	signComparison();
-
-	types();
-
-	printf("Remember what i learn (checkWork-function from code)\n");
-	checkWork();
-	*/
-
-	incrementDecrement();
-
-	printf("Distance\n");
-	arrayDiff();
-
-	printf("Vazhno\n");
-	signSpecific();
-
-	printf("for check\n");
-	forCheck();
-
-
-    return 0;
+	return 0;
 }
